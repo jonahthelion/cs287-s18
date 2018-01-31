502a65b17cdd2690e55124c39b86ccfa0de6ee99
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-3.local>
2018-01-30 20:08:03 -0500

    train

--- HW1/train.py
+++ HW1/train.py
@@ -8,3 +8,9 @@ LABEL = torchtext.data.Field(sequential=False)
 train, val, test = torchtext.datasets.SST.splits(
     TEXT, LABEL,
     filter_pred=lambda ex: ex.label != 'neutral')
+
+TEXT.build_vocab(train)
+LABEL.build_vocab(train)
+
+train_iter, val_iter, test_iter = torchtext.data.BucketIterator.splits(
+    (train, val, test), batch_size=10, device=-1)
