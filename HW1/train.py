e7df2c776e937530c1a14a3d88cac6c71108b1f8
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-3.local>
2018-01-31 18:30:10 -0500

    psetting

--- HW1/train.py
+++ HW1/train.py
@@ -2,8 +2,11 @@ import torchtext
 from torchtext.vocab import Vectors, GloVe
 from torch.autograd import Variable
 
-train_iter, val_iter, test_iter = get_data(batch_size=10)
+TEXT, LABEL, train_iter, val_iter, test_iter = get_data(batch_size=10)
 
 print (len(train_iter))
 print (len(val_iter))
 print (len(test_iter))
+
+for batch_num,batch in enumerate(train_iter):
+    print(batch.shape)
