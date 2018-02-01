5033b8fae04f2e2cc25a1e85da2627cc542905c3
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-3.local>
2018-01-31 20:51:36 -0500

    MNB

--- HW1/train.py
+++ HW1/train.py
@@ -3,6 +3,8 @@ import torchtext
 from torchtext.vocab import Vectors, GloVe
 from torch.autograd import Variable
 
+from tqdm import tqdm
+
 from utils.preprocess import get_data
 from models.psetModels import MNB
 
@@ -13,6 +15,6 @@ TEXT, LABEL, train_iter, val_iter, test_iter = get_data(batch_size=10)
 
 model = MNB(V=len(TEXT.vocab))
 
-for batch_num,batch in enumerate(train_iter):
+for batch_num,batch in enumerate(tqdm(train_iter)):
     model.train_sample(batch.label.data - 1, batch.text.data)
     assert (False)
