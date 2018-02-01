e8e917985a22ba0ce33f4fb72f8c19cc2d9c976e
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-3.local>
2018-01-31 21:27:20 -0500

    MNB

--- HW1/models/psetModels.py
+++ HW1/models/psetModels.py
@@ -9,15 +9,20 @@ class MNB(nn.Module):
         self.V = V
 
         # initialize counts
-        self.w_counts = [torch.zeros(V), torch.zeros(V)]
-        self.label_counts = [0, 0]
+        self.w_counts = torch.zeros(2, V)
+        self.label_counts = torch.zeros(2)
 
     def train_sample(self, label, text):
-        large_label = label.view(1, -1) * torch.ones(text.shape[0], 1).long()
 
-        for lab in [0,1]:
-            c = Counter(text[large_label==lab].numpy())
-            for val in c.keys():
-                self.w_counts[lab][val] += c[val]
-            self.label_counts[lab] += (label == lab).sum()
+        for phrase_ix in range(text.shape[1]):
+            c = Counter(text[:,phrase_ix].numpy())
+            for val in c:
+                self.w_counts[label[phrase_ix], val] += 1
+            self.label_counts[label[phrase_ix]] += 1
+
+    def postprocess(self):
+        self.w = nn.Linear(self.V, 1)
+        self.w.weight.data = torch.from_numpy( ( (self.label_counts[1] / self.label_counts[1].sum()) / (self.label_counts[0] / self.label_counts[0].sum()) ).log() ).float()
+        self.w.bias.data = torch.Tensor([self.label_counts[1] / float(self.label_counts[0])])
+
 
--- HW1/train.py
+++ HW1/train.py
@@ -2,6 +2,7 @@ import torch
 import torchtext
 from torchtext.vocab import Vectors, GloVe
 from torch.autograd import Variable
+import torch.nn as nn
 
 from tqdm import tqdm
 
@@ -18,3 +19,4 @@ model = MNB(V=len(TEXT.vocab))
 for epoch in range(1):
     for batch_num,batch in enumerate(tqdm(train_iter)):
         model.train_sample(batch.label.data - 1, batch.text.data)
+# model.postprocess()
