e6c5341b27919dd6b44619804ed6ef573262d09d
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-3.local>
2018-01-31 22:50:45 -0500

    writeup

--- HW1/models/psetModels.py
+++ HW1/models/psetModels.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from collections import Counter
+import nn.functional as F
 
 class MNB(nn.Module):
     def __init__(self, V, alpha):
