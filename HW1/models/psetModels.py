4b80902e9cf569c62c277887e7fae0e9b2bf0136
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-3.local>
2018-02-01 03:23:05 -0500

    MNB final

--- HW1/models/psetModels.py
+++ HW1/models/psetModels.py
@@ -128,9 +128,8 @@ class CBOW(nn.Module):
     def forward(self, text):
         if text.shape[1] == 1:
             print([text[:,0], torch.zeros(text.shape[0])])
-            text = torch.stack([text[:,0], torch.zeros(text.shape[0])], dim=1)
+            text = torch.stack([text[:,0], torch.zeros(text.shape[0]).long()], dim=1)
         embeds = torch.stack([self.embed(text[:,i]).mean(0) for i in range(text.shape[1])])
-        print(self.w(embeds).view(-1).shape)
         return self.w(embeds).view(-1)
 
     def train_sample(self, label, text, optimizer):
