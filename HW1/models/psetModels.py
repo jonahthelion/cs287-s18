f7be0a217f7b798b294b6833a03ec73ccd126d08
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-3.local>
2018-02-01 03:13:20 -0500

    MNB final

--- HW1/models/psetModels.py
+++ HW1/models/psetModels.py
@@ -126,7 +126,10 @@ class CBOW(nn.Module):
             )
 
     def forward(self, text):
-        embeds = torch.stack([self.embed(text[:,i]).mean(0) for i in range(text.shape[1])])
+        if text.shape[1] > 1:
+            embeds = torch.stack([self.embed(text[:,i]).mean(0) for i in range(text.shape[1])])
+        else:
+            print([self.embed(text[:,i]).mean(0) for i in range(text.shape[1])])
         return self.w(embeds).view(-1)
 
     def train_sample(self, label, text, optimizer):
@@ -147,8 +150,6 @@ class CBOW(nn.Module):
         print ('saving to', fname)
         upload = []
         for batch in test_iter:
-            print (batch.text.data)
-            print (self.forward(batch.text.data))
             probs = F.sigmoid(self.forward(batch.text.data)) + 1
             upload.extend(list(probs.data.numpy().round().astype(int).flatten()))
         with open(fname, 'w') as f:
