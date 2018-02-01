a68fa31001481913bd8eda7ed405bd3ad447e1f1
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-3.local>
2018-01-31 20:49:08 -0500

    MNB

--- HW1/models/psetModels.py
+++ HW1/models/psetModels.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from collections import Counter
 
 class MNB(nn.Module):
     def __init__(self, V):
@@ -8,10 +9,14 @@ class MNB(nn.Module):
         self.V = V
 
         # initialize counts
-        self.w_pos = torch.zeros(V)
-        self.w_neg = torch.zeros(V)
+        self.w_counts = [torch.zeros(V), torch.zeros(V)]
 
     def train_sample(label, text):
-        large_label = label.view(1, -1) * Variable(torch.ones(text.shape[0], 1).long())
+        large_label = label.view(1, -1) * torch.ones(text.shape[0], 1).long()
 
+        for lab in [0,1]:
+            c = Counter(text[large_label==lab].numpy())
+            print ('THING', lab,c)
+            for val in c.keys():
+                self.w_pos[lab][val] += c[val]
 
--- HW1/train.py
+++ HW1/train.py
@@ -1,3 +1,4 @@
+import torch
 import torchtext
 from torchtext.vocab import Vectors, GloVe
 from torch.autograd import Variable
@@ -13,4 +14,5 @@ TEXT, LABEL, train_iter, val_iter, test_iter = get_data(batch_size=10)
 model = MNB(V=len(TEXT.vocab))
 
 for batch_num,batch in enumerate(train_iter):
+    model.train_sample(batch.label.data - 1, batch.text.data)
     assert (False)
