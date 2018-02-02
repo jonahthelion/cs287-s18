260e107c186e0c85a5b52f171250b895dd8b87be
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-3.local>
2018-02-02 01:33:51 -0500

    alpha

--- HW1/train.py
+++ HW1/train.py
@@ -64,7 +64,7 @@ if chosen_model['type'] == 'log_reg':
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
 
-    for epoch in range(10):
+    for epoch in range(30):
         for batch_num,batch in enumerate(train_iter):
             optimizer.zero_grad()
             preds = model(batch.text.data)
