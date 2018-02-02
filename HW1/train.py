d72396dd201410b69959d81554e8c10908ca945c
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-3.local>
2018-02-02 01:54:14 -0500

    alpha

--- HW1/train.py
+++ HW1/train.py
@@ -64,7 +64,7 @@ if chosen_model['type'] == 'log_reg':
     model = LogReg(V=len(TEXT.vocab))
     model.cuda()
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0005, weight_decay=5e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
 
     for epoch in range(100):
         for batch_num,batch in enumerate(train_iter):
