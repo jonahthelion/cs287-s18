f33ffe1f076f728ce416b98de632c1e0316404ce
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-3.local>
2018-02-02 03:18:35 -0500

    alpha

--- HW1/train.py
+++ HW1/train.py
@@ -102,7 +102,7 @@ if chosen_model['type'] == 'log_reg':
 if chosen_model['type'] == 'CBOW':
     model = CBOW(V=len(TEXT.vocab), embed=TEXT.vocab.vectors)
     model.cuda()
-    optimizer = torch.optim.Adam(model.w.parameters(), lr=0.00001, weight_decay=5e-4)
+    optimizer = torch.optim.Adam(model.w.parameters(), lr=0.00001)
     for epoch in range(500):
         for batch_num,batch in enumerate(train_iter):
             model.train()
