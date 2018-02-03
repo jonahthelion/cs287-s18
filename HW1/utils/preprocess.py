0dcbf2bb95f56f1584b49e18c34d9c3bc9a011a8
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-3.local>
2018-02-02 21:15:10 -0500

    pcis

--- HW1/utils/preprocess.py
+++ HW1/utils/preprocess.py
@@ -44,7 +44,7 @@ def text_to_img(text, TEXT):
     sample_ix = 0
 
     img_text = textwrap.wrap(" ".join(TEXT.vocab.itos[ix] for ix in text[:,sample_ix]), 25)
-    img_text = [" "*np.random.randint(0, 25 - len(row)) + row for row in img_text]
+    img_text = [" "*np.random.randint(0, 26 - len(row)) + row for row in img_text]
     img_text = "\n".join(img_text)
 
     img = Image.new('RGBA', (200, 200), (120,20,20))
