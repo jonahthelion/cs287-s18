18ec66c706f0f91e6e0bd2b36ea768d5be432df7
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-3.local>
2018-02-02 21:41:22 -0500

    pcis

--- HW1/utils/preprocess.py
+++ HW1/utils/preprocess.py
@@ -44,10 +44,9 @@ def text_to_img(text, TEXT):
     sample_ix = 0
 
     img_text = textwrap.wrap(" ".join(TEXT.vocab.itos[ix] for ix in text[:,sample_ix]), 34)
-    img_text = [" "*np.random.randint(0, 35 - len(row)) + row + "\n" for row in img_text]
+    # img_text = [" "*np.random.randint(0, 35 - len(row)) + row + "\n" for row in img_text]
     print(img_text)
-    if len(img_text) < 4:
-        img_text =  "".join(img_text)
+    img_text =  "".join(img_text)
 
     img = Image.new('RGBA', (200, 200), (120,20,20))
     draw = ImageDraw.Draw(img)
