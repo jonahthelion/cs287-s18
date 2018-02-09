91b34943e77c705a31350a2d2f87e2dd40a1ea5e
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-02-08 20:55:41 -0500

    data

--- HW2/train.py
+++ HW2/train.py
@@ -1,8 +1,20 @@
 import torchtext
 from torchtext.vocab import Vectors
 
+max_size = 1000
+batch_size = 10
+bptt_len = 20
+
 TEXT = torchtext.data.Field()
 
 train, val, test = torchtext.datasets.LanguageModelingDataset.splits(
     path="PSET", 
     train="train.txt", validation="valid.txt", test="valid.txt", text_field=TEXT)
+
+TEXT.build_vocab(train, max_size=max_size)
+
+train_iter, val_iter, test_iter = torchtext.data.BPTTIterator.splits(
+    (train, val, test), batch_size=batch_size, device=-1, bptt_len=bptt_len, repeat=False)
+
+for batch in train_iter:
+    assert False
