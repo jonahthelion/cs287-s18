7c2e5a1b6c43e7ea07c5bb2395372a0bb8076329
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-02-08 20:49:02 -0500

    data

--- HW2/train.py
+++ HW2/train.py
@@ -1,1 +1,8 @@
-hello
+import torchtext
+from torchtext.vocab import Vectors
+
+TEXT = torchtext.data.Field()
+
+train, val, test = torchtext.datasets.LanguageModelingDataset.splits(
+    path="PSET", 
+    train="train.txt", validation="valid.txt", test="valid.txt", text_field=TEXT)
