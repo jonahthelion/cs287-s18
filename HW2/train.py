dd0b43bac00456162c3a8a12fdcbbb4858e1e5a5
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-02-08 21:51:49 -0500

    data

--- HW2/train.py
+++ HW2/train.py
@@ -1,9 +1,12 @@
 import torchtext
 from torchtext.vocab import Vectors
 
+from utils.models import TriGram
+
 max_size = 10002 # max is 10001
 batch_size = 10
 bptt_len = 32
+model_dict = {'type': 'trigram', 'alpha': [.1, .5, .4]}
 
 TEXT = torchtext.data.Field()
 
@@ -15,6 +18,9 @@ TEXT.build_vocab(train, max_size=max_size)
 
 train_iter, val_iter, test_iter = torchtext.data.BPTTIterator.splits(
     (train, val, test), batch_size=batch_size, device=-1, bptt_len=bptt_len, repeat=False)
+
+if model_dict['type'] == 'trigram':
+    model = TriGram(model_dict)
 
 for batch in train_iter:
     assert False
--- HW2/utils/models.py
+++ HW2/utils/models.py
@@ -1,1 +1,9 @@
-hello
+import torch
+import torch.nn as nn
+
+class TriGram(nn.Module):
+    def __init__(self, model_dict):
+        super(TriGram, self).__init__()
+
+        self.alpha = model_dict['alpha']
+        assert(sum(self.alpha) == 1), self.alpha
