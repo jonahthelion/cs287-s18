ac23e62685b003ad74e5c35fbbd29356621ee8ef
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-02-09 02:56:25 -0500

    data

--- HW2/train.py
+++ HW2/train.py
@@ -7,31 +7,30 @@ from utils.models import TriGram
 from utils.preprocess import get_data, get_model
 from utils.postprocess import evaluate, write_submission
 
-for alpha1 in [.1]:
-    for alpha2 in [.05]:
-        # NOTE: success of TriGram will depend weakly on batch size
-        model_dict = {'max_size': 100, # max is 10001
-                        'batch_size': 10, 
-                        'bptt_len': 32,
-                        'num_epochs': 1,
+for alpha1 in []
+# NOTE: success of TriGram will depend weakly on batch size
+model_dict = {'max_size': 100, # max is 10001
+                'batch_size': 10, 
+                'bptt_len': 32,
+                'num_epochs': 1,
 
-                        'output': 'simple1.txt',
+                'output': 'simple0.txt',
 
-                        'type': 'trigram', 
-                        'alpha': [alpha1, alpha2, 1-alpha1-alpha2],}
+                'type': 'trigram', 
+                'alpha': [.1, .5, .4],}
 
-        train_iter, val_iter, test_iter, TEXT = get_data(model_dict)
+train_iter, val_iter, test_iter, TEXT = get_data(model_dict)
 
-        model = get_model(model_dict)
+model = get_model(model_dict)
 
-        for epoch in range(model_dict['num_epochs']):
-            for batch in tqdm(train_iter):
-                model.train()
-                preds = model.train_predict(batch.text.cuda())
-        model.postprocess()
+for epoch in range(model_dict['num_epochs']):
+    for batch in tqdm(train_iter):
+        model.train()
+        preds = model.train_predict(batch.text.cuda())
+model.postprocess()
 
-        nll_l, MAP = evaluate(model, test_iter)
-        print(model_dict['alpha'])
-        print(nll_l, MAP)
-        samples, top_ranks = write_submission(model, model_dict['output'], TEXT)
+nll_l, MAP = evaluate(model, test_iter)
+print(model_dict['alpha'])
+print(nll_l, MAP)
+# samples, top_ranks = write_submission(model, model_dict['output'], TEXT)
 
