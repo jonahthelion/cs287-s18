028c3772c64df707bfc340bcf8e7824e497b39e9
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-02-10 16:51:23 -0500

    checking

--- HW2/train.py
+++ HW2/train.py
@@ -21,6 +21,8 @@ model_dict = {'max_size': 100, # max is 10001
 
 train_iter, val_iter, test_iter, TEXT = get_data(model_dict)
 
+assert False
+
 model = get_model(model_dict)
 
 for epoch in range(model_dict['num_epochs']):
