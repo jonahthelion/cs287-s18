60321cf3e93ac24232fc1a3b6dab60b8084ef624
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-02-13 12:05:59 -0500

    vis

--- HW2/train.py
+++ HW2/train.py
@@ -15,8 +15,8 @@ from utils.postprocess import evaluate, write_submission, vis_display
 # NOTE: success of TriGram will depend weakly on batch size
 model_dict = {'max_size': 10001, # max is 10001
                 'batch_size': 41, 
-                'bptt_len': 4,
-                'num_epochs': 5000,
+                'bptt_len': 32,
+                'num_epochs': 1,
 
                 'output': 'simple0.txt',
 
