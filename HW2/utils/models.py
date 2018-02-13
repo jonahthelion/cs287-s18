0173153cf7cc79fde1a905ee15c8048e2a1ac6cf
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-02-13 01:36:32 -0500

    last try

--- HW2/utils/models.py
+++ HW2/utils/models.py
@@ -73,11 +73,11 @@ class NN(nn.Module):
             )
 
     def train_predict(self, text):
-        probs = torch.zeros(text.shape[1], 300*3).cuda()
-        embeds = self.embed(text[-3:]).data
+        probs = Variable(torch.zeros(text.shape[1], 300*3).cuda())
+        embeds = self.embed(text[-3:])
 
-        for i in range(text.shape[1]):
-            probs[i] += embeds[:,i].contiguous().view(-1)
+        # for i in range(text.shape[1]):
+        #     probs[i] += embeds[:,i].contiguous().view(-1)
         return probs, embeds
 
 
