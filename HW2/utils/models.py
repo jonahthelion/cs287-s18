5219cd2eab470430e5453cf4f88531a40a26b218
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-02-17 10:56:48 -0500

    writeup

--- HW2/utils/models.py
+++ HW2/utils/models.py
@@ -80,7 +80,7 @@ class TriGram(nn.Module):
 model_dict = {'max_size': 10001, # max is 10001
                 'batch_size': 140, 
                 'bptt_len': 6,
-                'num_epochs': 4,
+                'num_epochs': 5,
 
                 'output': 'simple3.txt',
 
