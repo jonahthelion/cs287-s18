29b60b6abefd0ec435c64f27aa901dfd8e35d3b0
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-02-17 15:14:48 -0500

    writeup

--- HW2/train.py
+++ HW2/train.py
@@ -22,41 +22,41 @@ vis.env = 'train'
 
 train_iter, val_iter, test_iter, TEXT = get_data(model_dict)
 
-# model = get_model(model_dict)
+model = get_model(model_dict)
 
-# trainable = False
-# if len(list(model.parameters())) > 0:
-#     optimizer = torch.optim.Adam(model.parameters(), lr=0.0003)
-#     trainable = True
+trainable = False
+if len(list(model.parameters())) > 0:
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0003)
+    trainable = True
 
-# for epoch in range(model_dict['num_epochs']):
-#     for batch_num,batch in enumerate(tqdm(train_iter)):
-#         if trainable:
-#             if len(batch.text) < model_dict['lookback'] + 1:
-#                 continue
-#             model.train()
-#             optimizer.zero_grad()
+for epoch in range(model_dict['num_epochs']):
+    for batch_num,batch in enumerate(tqdm(train_iter)):
+        if trainable:
+            if len(batch.text) < model_dict['lookback'] + 1:
+                continue
+            model.train()
+            optimizer.zero_grad()
 
-#         probs = model.train_predict(batch.text.cuda())
+        probs = model.train_predict(batch.text.cuda())
 
-#         if trainable:
-#             actuals = batch.text[-1].cuda()
-#             loss = F.cross_entropy(probs, actuals)
-#             loss.backward()
-#             optimizer.step()
+        if trainable:
+            actuals = batch.text[-1].cuda()
+            loss = F.cross_entropy(probs, actuals)
+            loss.backward()
+            optimizer.step()
             
-#             if batch_num % 100 == 0:
-#                 loss_l = loss.data.cpu().numpy()[0]
-#                 if batch_num % 250 == 0:
-#                     model.eval()
-#                     MAP = evaluate(model, val_iter)
-#                     print(epoch, batch_num, lookback, MAP)
-#                 vis_windows = vis_display(vis, vis_windows, epoch + batch_num/float(len(train_iter)), loss_l, MAP)
+            if batch_num % 100 == 0:
+                loss_l = loss.data.cpu().numpy()[0]
+                if batch_num % 250 == 0:
+                    model.eval()
+                    MAP = evaluate(model, val_iter)
+                    print(epoch, batch_num, lookback, MAP)
+                vis_windows = vis_display(vis, vis_windows, epoch + batch_num/float(len(train_iter)), loss_l, MAP)
 
-# model.postprocess()
-# model.eval()
-MAP = evaluate(ensemb, val_iter)
-print("MAP FINAL", bestmap)
+model.postprocess()
+model.eval()
+MAP = evaluate(model, val_iter)
+print("MAP FINAL", MAP)
 
 #############################
 
