0de445491ecd0716b8d65c4562e4576a447a34b5
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-02-17 15:48:22 -0500

    writeup

--- HW2/train.py
+++ HW2/train.py
@@ -50,7 +50,7 @@ for epoch in range(model_dict['num_epochs']):
                 if batch_num % 250 == 0:
                     model.eval()
                     MAP = evaluate(model, val_iter)
-                    print(epoch, batch_num, lookback, MAP)
+                    print(epoch, batch_num, MAP)
                 vis_windows = vis_display(vis, vis_windows, epoch + batch_num/float(len(train_iter)), loss_l, MAP)
 
 model.postprocess()
