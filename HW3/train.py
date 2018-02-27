98a16ec777213c926a74dedcec16b59c2ca19c46
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-02-27 15:03:08 -0500

    train

---
+++ HW3/train.py
@@ -0,0 +1,17 @@
+import torchtext
+import torch
+from torchtext.vocab import Vectors
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+from tqdm import tqdm
+import numpy as np
+import visdom
+
+
+# visdom
+vis_windows = None
+vis = visdom.Visdom()
+vis.env = 'train'
+
+#############################
