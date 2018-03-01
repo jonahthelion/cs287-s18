40f6de05922abd61d75b4872f8f2640a6431e576
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-03-01 14:09:14 -0500

    testing

--- HW3/train.py
+++ HW3/train.py
@@ -30,7 +30,8 @@ train_iter, val_iter, DE, EN = get_data(model_dict)
 
 ###########
 model = torch.load('noAttention.p')
-model.flatten_parameters()
+model.encoder.flatten_parameters()
+model.decoder.flatten_parameters()
 model.eval()
 fname = 'PSET/source_test.txt'
 with open(fname, 'rb') as reader:
