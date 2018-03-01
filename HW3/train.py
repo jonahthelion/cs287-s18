97f4eef18d2b42f7f8a8d014fd13db61715b5847
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-02-28 23:34:09 -0500

    data

--- HW3/train.py
+++ HW3/train.py
@@ -34,9 +34,8 @@ for epoch in range(model_dict['num_epochs']):
     for batch_num,batch in enumerate(train_iter):
         model.train()
         optimizer.zero_grad()
-        preds = model.train_predict(batch.src.cuda(), batch.trg.cuda())
-        assert False
-        loss = torch.cat([F.cross_entropy(preds[:-1][row_ix], batch.trg[1:][row_ix].cuda(), ignore_index=1) for row_ix in range(batch.trg.shape[0] - 1)]).mean()
+        preds = model.train_predict(batch.src.cuda(), batch.trg.cuda())[:-1]
+        loss = F.cross_entropy(preds, batch.trg[1:].cuda(), ignore_index=1)
         loss.backward()
         optimizer.step()
 
