c64335d2a6f882f0df999366cec25a8bff09ba89
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-03-01 10:43:11 -0500

    data

--- HW3/utils/models.py
+++ HW3/utils/models.py
@@ -24,8 +24,7 @@ class noAttention(nn.Module):
 
     def get_encode(self, src):
         embedded = self.embedder(src)
-        output = embedded
-        output, hidden = self.encoder(output, (torch.zeros(self.num_encode, src.shape[0], self.D), torch.zeros(self.num_encode, src.shape[0], self.D)))
+        output, hidden = self.encoder(embedded, (torch.zeros(self.num_encode, src.shape[0], self.D), torch.zeros(self.num_encode, src.shape[0], self.D)))
         return output, hidden
 
     def get_decode(self, trg, hidden):
