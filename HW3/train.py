77979300b1c4c248cacdd93026dbfb9eb0b6d508
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-02-28 23:02:55 -0500

    data

--- HW3/train.py
+++ HW3/train.py
@@ -9,6 +9,7 @@ import numpy as np
 import visdom
 
 from utils.preprocess import get_data, get_model
+from utils.postprocess import vis_display, evaluate
 
 # visdom
 vis_windows = None
@@ -20,15 +21,24 @@ vis.env = 'train'
 model_dict = {'type': 'noAttention',
                 'D': 200,
                 'num_encode': 4,
-                'num_decode': 4}
+                'num_decode': 4,
+                'num_epochs': 10}
 
 train_iter, val_iter, DE, EN = get_data(model_dict)
 
 model = get_model(model_dict, DE, EN)
 
-for batch in train_iter:
-    model.train()
-    preds = model.train_predict(batch.src.cuda(), batch.trg.cuda())
-    loss = F.cross_entropy(preds[:-1].view(-1,preds.shape[-1]), batch.trg[1:].view(-1).cuda(), ignore_index=1)
-    
-    print(loss)
+for epoch in range(model_dict['num_epochs']):
+    for batch_num,batch in enumerate(train_iter):
+        model.train()
+        preds = model.train_predict(batch.src.cuda(), batch.trg.cuda())
+        loss = F.cross_entropy(preds[:-1].view(-1,preds.shape[-1]), batch.trg[1:].view(-1).cuda(), ignore_index=1)
+        
+        if batch_num % 100 == 0:
+            loss_t = loss.data.cpu().numpy()[0]
+            loss_v = None
+            if batch_num % 250 == 0:
+                model.eval()
+                loss_v = evaluate(model, val_iter)
+                print(epoch, batch_num, loss_v)
+            vis_windows = vis_display(vis, vis_windows, epoch + batch_num/float(len(train_iter)), loss_t, loss_v)
--- HW3/utils/postprocess.py
+++ HW3/utils/postprocess.py
@@ -5,3 +5,24 @@ from torch.autograd import Variable
 
 import numpy as np
 from tqdm import tqdm
+
+def vis_display(vis, vis_windows, x_coord, train_l, MAP):
+    if vis_windows is None:
+        vis_windows = {}
+        vis_windows['train_ce'] = vis.line(Y=torch.Tensor([float(train_l)]), X=torch.Tensor([x_coord]), opts=dict(title='Train CE'))
+        vis_windows['val_ce'] = vis.line(Y=torch.Tensor([float(MAP)]), X=torch.Tensor([x_coord]), opts=dict(title='Validation CE'))
+    else:
+        vis.line(Y=torch.Tensor([float(train_l)]), X=torch.Tensor([x_coord]), win=vis_windows['train_ce'], update='append', opts=dict(title='Train CE'))
+        vis.line(Y=torch.Tensor([float(MAP)]), X=torch.Tensor([x_coord]), win=vis_windows['val_ce'], update='append', opts=dict(title='Validation CE'))
+    return vis_windows
+
+def evaluate(model, val_iter):
+    all_losses = []
+    for batch in val_iter:
+        if batch.src.shape[1] == 32:
+            preds = model.train_predict(batch.src.cuda(), batch.trg.cuda())
+            loss = F.cross_entropy(preds[:-1].view(-1,preds.shape[-1]), batch.trg[1:].view(-1).cuda(), ignore_index=1)
+            all_losses.append(loss)
+    loss_v = torch.cat(all_losses).mean().data.cpu().numpy()[0]
+
+    return loss_v
