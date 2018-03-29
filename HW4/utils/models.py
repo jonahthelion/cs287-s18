7719f4749981101f01f181c2e75f4a347e8ecf8a
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-03-29 01:19:28 -0400

    VAE

--- HW4/utils/models.py
+++ HW4/utils/models.py
@@ -5,9 +5,18 @@ class SimpleVAE(nn.Module):
     def __init__(self, args):
         super(SimpleVAE, self).__init__()
         self.hidden = args.hidden
-        self.encoder = nn.Linear(28*28, 2*self.hidden)
+        self.encoder = nn.Sequential(
+                        nn.Linear(28*28, 14*14),
+                        nn.ReLU(),
+                        nn.Linear(14*14, 2*self.hidden),
+                    )
 
-        self.decoder = nn.Linear(self.hidden, 28*28)
+
+        self.decoder = nn.Sequential(
+                        nn.Linear(self.hidden, 14*14),
+                        nn.ReLU(),
+                        nn.Linear(14*14, 28*28),
+                    )
 
     def get_encoding(self, x):
         out = self.encoder(x.view(x.shape[0], 28*28))
