004b1c3cc1f2b951f490313e1cef7c557066d86d
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-03-28 21:40:53 -0400

    hw 4

--- HW4/VAE.py
+++ HW4/VAE.py
@@ -5,18 +5,33 @@ import torchvision.transforms as transforms
 train_dataset = datasets.MNIST(root='./data/',
                             train=True, 
                             transform=transforms.ToTensor(),
-                            download=True)
+                            download=False)
 test_dataset = datasets.MNIST(root='./data/',
                            train=False, 
                            transform=transforms.ToTensor())
-
-print(len(train_dataset))
-print(len(test_dataset))
 
 torch.manual_seed(3435)
 train_img = torch.stack([torch.bernoulli(d[0]) for d in train_dataset])
 train_label = torch.LongTensor([d[1] for d in train_dataset])
 test_img = torch.stack([torch.bernoulli(d[0]) for d in test_dataset])
 test_label = torch.LongTensor([d[1] for d in test_dataset])
-print(train_img[0])
-print(train_img.size(), train_label.size(), test_img.size(), test_label.size())
+
+val_img = train_img[-10000:].clone()
+val_label = train_label[-10000:].clone()
+train_img = train_img[:-10000]
+train_label = train_label[:-10000]
+
+
+train = torch.utils.data.TensorDataset(train_img, train_label)
+val = torch.utils.data.TensorDataset(val_img, val_label)
+test = torch.utils.data.TensorDataset(test_img, test_label)
+BATCH_SIZE = 100
+train_loader = torch.utils.data.DataLoader(train, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test, batch_size=BATCH_SIZE, shuffle=True)
+
+
+for datum in train_loader:
+    img, label = datum
+    print(img.size(), label.size())
+    break
