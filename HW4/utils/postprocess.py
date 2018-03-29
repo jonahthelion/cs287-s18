2a2bedb5a4fe18977b60d7bcb1dca79b8271a98b
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-03-29 02:27:47 -0400

    VAE

--- HW4/utils/postprocess.py
+++ HW4/utils/postprocess.py
@@ -7,11 +7,12 @@ def vis_display(vis, vis_windows, x_coord, train_l, MAP, sample_img, val_kl, val
         vis_windows['val_ce'] = vis.line(Y=torch.Tensor([float(MAP)]), X=torch.Tensor([x_coord]), opts=dict(title='Train KL', ytickmax=0.5, ytickmin=0))
         vis_windows['val_recon'] = vis.line(Y=torch.Tensor([float(val_recon)]), X=torch.Tensor([x_coord]), opts=dict(title='Validation XENT', ytickmax=0.5, ytickmin=0))
         vis_windows['val_kl'] = vis.line(Y=torch.Tensor([float(val_kl)]), X=torch.Tensor([x_coord]), opts=dict(title='Validation KL', ytickmax=0.5, ytickmin=0))
-        vis_windows['imgs'] = vis.images(sample_img)
+        vis_windows['imgs'] = vis.images(sample_img, nrow=4)
     else:
         vis.line(Y=torch.Tensor([float(train_l)]), X=torch.Tensor([x_coord]), win=vis_windows['train_ce'], update='append', opts=dict(title='Train XENT', ytickmax=0.5, ytickmin=0))
         vis.line(Y=torch.Tensor([float(MAP)]), X=torch.Tensor([x_coord]), win=vis_windows['val_ce'], update='append', opts=dict(title='Train KL', ytickmax=0.5, ytickmin=0))
-        vis.line(Y=torch.Tensor([float(val_recon)]), X=torch.Tensor([x_coord]), win=vis_windows['val_recon'], update='append', opts=dict(title='Validation XENT', ytickmax=0.5, ytickmin=0))
-        vis.line(Y=torch.Tensor([float(val_kl)]), X=torch.Tensor([x_coord]), win=vis_windows['val_kl'], update='append', opts=dict(title='Validation KL', ytickmax=0.5, ytickmin=0))
-        vis.images(sample_img, win=vis_windows['imgs'])
+        if not val_kl is None:
+            vis.line(Y=torch.Tensor([float(val_recon)]), X=torch.Tensor([x_coord]), win=vis_windows['val_recon'], update='append', opts=dict(title='Validation XENT', ytickmax=0.5, ytickmin=0))
+            vis.line(Y=torch.Tensor([float(val_kl)]), X=torch.Tensor([x_coord]), win=vis_windows['val_kl'], update='append', opts=dict(title='Validation KL', ytickmax=0.5, ytickmin=0))
+            vis.images(sample_img, win=vis_windows['imgs'], nrow=4)
     return vis_windows
