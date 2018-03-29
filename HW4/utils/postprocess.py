bcf1430cab04864b4dfa22307670afef59047a50
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-03-29 01:32:33 -0400

    VAE

--- HW4/utils/postprocess.py
+++ HW4/utils/postprocess.py
@@ -3,9 +3,9 @@ import torch
 def vis_display(vis, vis_windows, x_coord, train_l, MAP):
     if vis_windows is None:
         vis_windows = {}
-        vis_windows['train_ce'] = vis.line(Y=torch.Tensor([float(2**train_l)]), X=torch.Tensor([x_coord]), opts=dict(title='Train XENT'))
-        vis_windows['val_ce'] = vis.line(Y=torch.Tensor([float(2**MAP)]), X=torch.Tensor([x_coord]), opts=dict(title='Train KL'))
+        vis_windows['train_ce'] = vis.line(Y=torch.Tensor([float(train_l)]), X=torch.Tensor([x_coord]), opts=dict(title='Train XENT', ytickmax=0.5, ytickmin=0))
+        vis_windows['val_ce'] = vis.line(Y=torch.Tensor([float(MAP)]), X=torch.Tensor([x_coord]), opts=dict(title='Train KL', ytickmax=0.5, ytickmin=0))
     else:
-        vis.line(Y=torch.Tensor([float(2**train_l)]), X=torch.Tensor([x_coord]), win=vis_windows['train_ce'], update='append', opts=dict(title='Train XENT'))
-        vis.line(Y=torch.Tensor([float(2**MAP)]), X=torch.Tensor([x_coord]), win=vis_windows['val_ce'], update='append', opts=dict(title='Train KL'))
+        vis.line(Y=torch.Tensor([float(train_l)]), X=torch.Tensor([x_coord]), win=vis_windows['train_ce'], update='append', opts=dict(title='Train XENT', ytickmax=0.5, ytickmin=0))
+        vis.line(Y=torch.Tensor([float(MAP)]), X=torch.Tensor([x_coord]), win=vis_windows['val_ce'], update='append', opts=dict(title='Train KL', ytickmax=0.5, ytickmin=0))
     return vis_windows
