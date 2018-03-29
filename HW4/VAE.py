cb326c16368bb01442c4b799ed69de88cbb29d15
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-03-29 03:09:42 -0400

    VAE

--- HW4/VAE.py
+++ HW4/VAE.py
@@ -11,7 +11,7 @@ from utils.preprocess import get_data, get_model
 from utils.postprocess import vis_display
 
 """
-python VAE.py -model "Simple" -hidden 2 -lr .0005 -epochs 20 -kl_lam 0.1
+python VAE.py -model "Simple" -hidden 2 -lr .0005 -epochs 20 -kl_lam 0.5
 """
 
 # visdom
