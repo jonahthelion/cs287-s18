0ce5f806b556e1e0af8952ace693c995be070013
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-03-30 00:22:55 -0400

    mod gan

--- HW4/VAE.py
+++ HW4/VAE.py
@@ -70,76 +70,77 @@ for epoch in range(args.epochs):
             if data_ix % 150 == 0:
                 val_kl, val_reconstruct = get_validation_loss(model, val_loader)
             vis_windows = vis_display(vis, vis_windows, epoch + data_ix/float(len(train_loader)), l_reconstruct.data.cpu()[0], l_kl.data.cpu()[0], F.sigmoid(sample_img).data.cpu().unsqueeze(1), val_kl, val_reconstruct)
+torch.save(model, 'VAEconv.p')
+modelname = 'VAEconv.p'
+### Scatter Plot
+model = torch.load(modelname)
+model.eval()
+all_mu = []; all_labels = [];
+for data in test_loader:
+    img, label = data
+    mu, sig = model.get_encoding(Variable(img).cuda())
+    all_mu.append(mu.data.cpu())
+    all_labels.append(label)
+all_mu = torch.cat(all_mu, 0); all_labels = torch.cat(all_labels, 0)
+fig = plt.figure()
+handles = []
+for dig in range(10):
+    ixes = [ix for ix in range(len(all_mu)) if all_labels[ix] == dig]
+    p = plt.plot([all_mu[ix,0] for ix in ixes], [all_mu[ix,1] for ix in ixes], '.')
+    handles.append(mpl.patches.Patch(color=p[0].get_color(), label=str(dig)))
+plt.legend(handles=handles)
+plt.tight_layout()
+plt.savefig('vae_mus2.pdf')
 
-#### Scatter Plot
-# model = torch.load('VAE.p')
-# model.eval()
-# all_mu = []; all_labels = [];
-# for data in test_loader:
-#     img, label = data
-#     mu, sig = model.get_encoding(Variable(img).cuda())
-#     all_mu.append(mu.data.cpu())
-#     all_labels.append(label)
-# all_mu = torch.cat(all_mu, 0); all_labels = torch.cat(all_labels, 0)
-# fig = plt.figure()
-# handles = []
-# for dig in range(10):
-#     ixes = [ix for ix in range(len(all_mu)) if all_labels[ix] == dig]
-#     p = plt.plot([all_mu[ix,0] for ix in ixes], [all_mu[ix,1] for ix in ixes], '.')
-#     handles.append(mpl.patches.Patch(color=p[0].get_color(), label=str(dig)))
-# plt.legend(handles=handles)
-# plt.tight_layout()
-# plt.savefig('vae_mus2.pdf')
+#### Interpolation
+model = torch.load(modelname)
+model.eval()
+for data in test_loader:
+    for ixstart in range(10):
+        img, label = data
+        mu, sig = model.get_encoding(Variable(img).cuda())
+        fig = plt.figure(figsize=(10,2))
+        gs = mpl.gridspec.GridSpec(1, 12)
+        ax = plt.subplot(gs[0,0])
+        plt.imshow(img[ixstart][0].numpy(), cmap='Greys')
+        frame1 = plt.gca()
+        frame1.axes.xaxis.set_ticklabels([])
+        frame1.axes.yaxis.set_ticklabels([])
+        ax = plt.subplot(gs[0,-1])
+        plt.imshow(img[ixstart+1][0].numpy(), cmap='Greys')
+        frame1 = plt.gca()
+        frame1.axes.xaxis.set_ticklabels([])
+        frame1.axes.yaxis.set_ticklabels([])
+        diff = mu[ixstart+1]-mu[ixstart]
+        for push_ix,push in enumerate(np.linspace(0,1,10)):
+            ax = plt.subplot(gs[0,push_ix+1])
+            plt.imshow(F.sigmoid(model.get_decoding((mu[ixstart] + diff*push).unsqueeze(0))[0]).data.cpu().numpy(), cmap='Greys')
+            frame1 = plt.gca()
+            frame1.axes.xaxis.set_ticklabels([])
+            frame1.axes.yaxis.set_ticklabels([])
+        plt.tight_layout()
+        plt.savefig('interp_vae' + str(ixstart) + '.pdf')
+        plt.close(fig)
+    break
 
-# #### Interpolation
-# model = torch.load('VAE.p')
-# model.eval()
-# for data in test_loader:
-#     for ixstart in range(10):
-#         img, label = data
-#         mu, sig = model.get_encoding(Variable(img).cuda())
-#         fig = plt.figure(figsize=(10,2))
-#         gs = mpl.gridspec.GridSpec(1, 12)
-#         ax = plt.subplot(gs[0,0])
-#         plt.imshow(img[ixstart][0].numpy(), cmap='Greys')
-#         frame1 = plt.gca()
-#         frame1.axes.xaxis.set_ticklabels([])
-#         frame1.axes.yaxis.set_ticklabels([])
-#         ax = plt.subplot(gs[0,-1])
-#         plt.imshow(img[ixstart+1][0].numpy(), cmap='Greys')
-#         frame1 = plt.gca()
-#         frame1.axes.xaxis.set_ticklabels([])
-#         frame1.axes.yaxis.set_ticklabels([])
-#         diff = mu[ixstart+1]-mu[ixstart]
-#         for push_ix,push in enumerate(np.linspace(0,1,10)):
-#             ax = plt.subplot(gs[0,push_ix+1])
-#             plt.imshow(F.sigmoid(model.get_decoding((mu[ixstart] + diff*push).unsqueeze(0))[0]).data.cpu().numpy(), cmap='Greys')
-#             frame1 = plt.gca()
-#             frame1.axes.xaxis.set_ticklabels([])
-#             frame1.axes.yaxis.set_ticklabels([])
-#         plt.tight_layout()
-#         plt.savefig('interp_vae' + str(ixstart) + '.pdf')
-#         plt.close(fig)
-#     break
-
-# #### Mesh Grid
-# model = torch.load('VAE.p')
-# model.eval()
-# fig = plt.figure()
-# gs = mpl.gridspec.GridSpec(15, 15)
-# gs.update(wspace=0.025, hspace=0.025)
-# grid = np.linspace(-2, 2, 15)
-# for i in range(15):
-#     for j in range(15):
-#         img = F.sigmoid(model.get_decoding(Variable(torch.Tensor([grid[i], grid[j]])).cuda().unsqueeze(0))[0].data.cpu()).numpy()
-#         ax = plt.subplot(gs[14-j, i])
-#         plt.imshow(img, cmap='Greys')
-#         ax.axes.xaxis.set_ticklabels([])
-#         ax.axes.yaxis.set_ticklabels([])
-#         plt.axis('off')
-# plt.tight_layout()
-# plt.savefig('gridvae.pdf')
-# plt.close(fig)
+#### Mesh Grid
+model = torch.load(modelname)
+model.eval()
+fig = plt.figure()
+gs = mpl.gridspec.GridSpec(15, 15)
+gs.update(wspace=0.025, hspace=0.025)
+grid = np.linspace(-2, 2, 15)
+for i in range(15):
+    for j in range(15):
+        img = F.sigmoid(model.get_decoding(Variable(torch.Tensor([grid[i], grid[j]])).cuda().unsqueeze(0))[0].data.cpu()).numpy()
+        ax = plt.subplot(gs[14-j, i])
+        plt.imshow(img, cmap='Greys')
+        ax.axes.xaxis.set_ticklabels([])
+        ax.axes.yaxis.set_ticklabels([])
+        plt.axis('off')
+plt.tight_layout()
+plt.savefig('gridvae.pdf')
+plt.close(fig)
 
 
 
