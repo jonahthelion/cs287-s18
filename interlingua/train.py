f51690c9d64d95aea5a04480dbc67df1dea5b87e
Jonah Philion <jonahphilion@Jonahs-MacBook-Pro-4.local>
2018-04-04 15:51:32 -0400

    data

--- interlingua/train.py
+++ interlingua/train.py
@@ -2,12 +2,12 @@ from torchtext import data
 from torchtext import datasets
 import spacy
 
-spacy_de = spacy.load('de')
+# spacy_de = spacy.load('de')
 spacy_en = spacy.load('en')
 spacy_fr = spacy.load('fr')
 
-def tokenize_de(text):
-    return [tok.text for tok in spacy_de.tokenizer(text)]
+# def tokenize_de(text):
+#     return [tok.text for tok in spacy_de.tokenizer(text)]
 
 def tokenize_en(text):
     return [tok.text for tok in spacy_en.tokenizer(text)]
@@ -18,7 +18,7 @@ def tokenize_fr(text):
 BOS_WORD = '<s>'
 EOS_WORD = '</s>'
 EN = data.Field(tokenize=tokenize_en, init_token = BOS_WORD, eos_token = EOS_WORD)
-DE = data.Field(tokenize=tokenize_de, init_token = BOS_WORD, eos_token = EOS_WORD)
+# DE = data.Field(tokenize=tokenize_de, init_token = BOS_WORD, eos_token = EOS_WORD)
 FR = data.Field(tokenize=tokenize_fr, init_token = BOS_WORD, eos_token = EOS_WORD)
 
-train, val, test = datasets.Multi30k.splits(exts=('.de', '.en', '.fr'), fields=(DE, EN, FR))
+train, val, test = datasets.Multi30k.splits(exts=('.fr', '.en'), fields=(FR, EN))
